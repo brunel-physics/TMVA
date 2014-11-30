df083d9fce430c58793c259783069df78e0f3428
kskovpen <Kirill.Skovpen@cern.ch>
2014-11-30 13:58:12 +0100

    update

--- trainingBDT.C
+++ trainingBDT.C
@@ -21,31 +21,34 @@ void trainingBDT(){
   //---------------------------------------------------------------
   // This loads the library
   TMVA::Tools::Instance();
-  TString outfileName( "trainingBDT_tZq.root" );
+  TString outfileName( "trainingBDT_FCNCzct.root" );
+//  TString outfileName( "trainingBDT_tZq.root" );
   TFile* outputFile = TFile::Open( outfileName, "RECREATE" );
   //TMVA::Factory *factory = new TMVA::Factory( "BDT_trainning_tzq", outputFile,"!V:!Silent:Color:DrawProgressBar:Transformations=I;D;P;G,D:AnalysisType=Classification" );
-  TMVA::Factory *factory = new TMVA::Factory( "BDT_trainning_tzq", outputFile,"!V:!Silent:Color:DrawProgressBar:Transformations=I:AnalysisType=Classification" );
+//  TMVA::Factory *factory = new TMVA::Factory( "BDT_trainning_tzq", outputFile,"!V:!Silent:Color:DrawProgressBar:Transformations=I:AnalysisType=Classification" );
+  TMVA::Factory *factory = new TMVA::Factory( "BDT_trainning_FCNCzct", outputFile,"!V:!Silent:Color:DrawProgressBar:Transformations=I:AnalysisType=Classification" );
   
   
   
-  TFile *input_sig      = TFile::Open( "../TreeReader/outputroot/histofile_tZq.root" );
+//  TFile *input_sig      = TFile::Open( "../TreeReader/outputroot/histofile_tZq.root" );
+  TFile *input_sig      = TFile::Open( "../TreeReader/outputroot/histofile_FCNCzct.root" );
   TFile *input_wz       = TFile::Open( "../TreeReader/outputroot/histofile_WZ.root" );
   
   
-  TTree *signal            = (TTree*)input_sig->Get("Ttree_tZq");
+//  TTree *signal            = (TTree*)input_sig->Get("Ttree_tZq");
+  TTree *signal            = (TTree*)input_sig->Get("Ttree_FCNCzct");
   TTree *background     = (TTree*)input_wz->Get("Ttree_WZ");
   
   factory->AddSignalTree    ( signal,	   1.);
   factory->AddBackgroundTree( background,  1.);
   
-  
   std::vector<TString > varList;
-  varList.push_back("tree_cosThetaStar");;
+//  varList.push_back("tree_cosThetaStar");;
   varList.push_back("tree_topMass");     
-  varList.push_back("tree_totMass");     
+//  varList.push_back("tree_totMass");     
   varList.push_back("tree_deltaPhilb");  
-  varList.push_back("tree_deltaRlb");    
-  varList.push_back("tree_deltaRTopZ");  
+//  varList.push_back("tree_deltaRlb");    
+//  varList.push_back("tree_deltaRTopZ");  
   varList.push_back("tree_asym");        
   varList.push_back("tree_Zpt");         
   varList.push_back("tree_ZEta");        
@@ -53,11 +56,11 @@ void trainingBDT(){
   varList.push_back("tree_topEta");      
   varList.push_back("tree_NJets");       
   varList.push_back("tree_NBJets");	 
-  varList.push_back("tree_deltaRZl");	 
+//  varList.push_back("tree_deltaRZl");	 
   varList.push_back("tree_deltaPhiZmet");
   varList.push_back("tree_btagDiscri");  
-  varList.push_back("tree_totPt");	
-  varList.push_back("tree_totEta");	
+//  varList.push_back("tree_totPt");	
+//  varList.push_back("tree_totEta");	
   varList.push_back("tree_leptWPt");	 
   varList.push_back("tree_leptWEta");	 
   varList.push_back("tree_leadJetPt");   
