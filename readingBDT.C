904b77c3559afee3a1434e419d5070d79a04d062
kskovpen <Kirill.Skovpen@cern.ch>
2014-11-30 13:58:08 +0100

    update

--- readingBDT.C
+++ readingBDT.C
@@ -41,17 +41,17 @@ void readingBDT(){
    //create the BDT reader
    TMVA::Reader *reader = new TMVA::Reader( "!Color:!Silent" );    
    
-   TFile *input         = new TFile( "../TreeReader/outputroot/histofile_tZq.root", "read");
-   TFile *target= new TFile( "HistoBDToutput/TMVApp_tZqsm.root","RECREATE" );
-   
+   TFile *input         = new TFile( "../TreeReader/outputroot/histofile_FCNCzct.root", "read");
+//   TFile *target= new TFile( "HistoBDToutput/TMVApp_tZqsm.root","RECREATE" );
+   TFile *target= new TFile( "HistoBDToutput/TMVApp_FCNCzct.root","RECREATE" );
    
   std::vector<TString > varList;
-  varList.push_back("tree_cosThetaStar");;
+//  varList.push_back("tree_cosThetaStar");;
   varList.push_back("tree_topMass");     
-  varList.push_back("tree_totMass");     
+//  varList.push_back("tree_totMass");     
   varList.push_back("tree_deltaPhilb");  
-  varList.push_back("tree_deltaRlb");    
-  varList.push_back("tree_deltaRTopZ");  
+//  varList.push_back("tree_deltaRlb");    
+//  varList.push_back("tree_deltaRTopZ");  
   varList.push_back("tree_asym");        
   varList.push_back("tree_Zpt");         
   varList.push_back("tree_ZEta");        
@@ -59,11 +59,11 @@ void readingBDT(){
   varList.push_back("tree_topEta");      
   varList.push_back("tree_NJets");       
   varList.push_back("tree_NBJets");	 
-  varList.push_back("tree_deltaRZl");	 
+//  varList.push_back("tree_deltaRZl");	 
   varList.push_back("tree_deltaPhiZmet");
   varList.push_back("tree_btagDiscri");  
-  varList.push_back("tree_totPt");	
-  varList.push_back("tree_totEta");	
+//  varList.push_back("tree_totPt");	
+//  varList.push_back("tree_totEta");	
   varList.push_back("tree_leptWPt");	 
   varList.push_back("tree_leptWEta");	 
   varList.push_back("tree_leadJetPt");   
@@ -80,7 +80,8 @@ void readingBDT(){
   for(unsigned int i=0; i< varList.size() ; i++) reader->AddVariable( varList[i].Data(),  &(treevars[i])  );
   
   
-  reader->BookMVA( "BDT", "weights/BDT_trainning_tzq_BDT.weights.xml" ); 
+  reader->BookMVA( "BDT", "weights/BDT_trainning_FCNCzct_BDT.weights.xml" );
+//  reader->BookMVA( "BDT", "weights/BDT_trainning_tzq_BDT.weights.xml" );
   
   
   
@@ -89,7 +90,8 @@ void readingBDT(){
    //for tZq signal
    //-----------------------------------------------------
   input->cd();
-  TTree* theTree = (TTree*)input->Get("Ttree_tZq");
+  TTree* theTree = (TTree*)input->Get("Ttree_FCNCzct");
+//  TTree* theTree = (TTree*)input->Get("Ttree_tZq");
   for (unsigned int ivar=0; ivar<varsize; ivar++) theTree->SetBranchAddress( varList[ivar].Data(), &(treevars[ivar]) );
   
   for(int i=0; i< theTree->GetEntries(); i++){
