cbf304ebadda91f8ca210cfb2eab3a670f95bcd0
Alexander <alexander.david.morton@cern.ch>
2015-06-11 16:19:58 +0100

    Updated the default output for training trees in theMVAtool.h

--- theMVAtool.h
+++ theMVAtool.h
@@ -48,7 +48,7 @@ public :
    
    
    void doTraining(TString indir = "inputroot/met0mtw0", TString channel = "all", TString bdtTrees = "100");
-   void doReading(TString indir = "inputroot/met0mtw0", TString outDir = "outputroot/", TString channel = "all", float bdtcut = 100.0);
+   void doReading(TString indir = "inputroot/met0mtw0", TString outDir = "outputroot/met0mtw0", TString channel = "all", float bdtcut = 100.0);
    
    void loopInSample(TFile* input, TString sample, float *treevars, float bdtcut, TString channel);
    
